b72f0a656e30f074ba3a156a8bcc841328961ca0
samnang-an <samnang.an7@gmail.com>
2024-01-30 23:09:07 -0600

    feat: ad client service

---
+++ ClientService/src/test/java/com/crqs/productquerycommandservice/ProductQueryCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crqs.productquerycommandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductQueryCommandServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
